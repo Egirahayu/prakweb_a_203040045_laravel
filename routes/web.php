01b2e394c84f17f136f9dfacb675da73c79c5bb9
Egirahayu <mohammadegirahayu>
2022-10-26 20:41:36 +0700

    Membuat post category dan eloquent relationship

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
@@ -39,3 +40,11 @@ Route::get('/blog', [PostController::class, 'index']);
 
 // Halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('category', [
+        'totle' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
