3408f7f479b8db0fdfc8b509e9d8eac14bfe296f
Egirahayu <mohammadegirahayu>
2022-10-31 22:11:36 +0700

    Membuat searching dan pagination

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
 // NRP : 203040045
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger ">
   <div class="container">
     <a class="navbar-brand" href="/">ER Blog</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,16 +12,16 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link {{ ($title === "home") ? 'active' : ''}}" href="/">Home</a>
+          <a class="nav-link {{ ($active === "home") ? 'active' : ''}}" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "about") ? 'active' : ''}}" href="/about">About</a>
+          <a class="nav-link {{ ($active === "about") ? 'active' : ''}}" href="/about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "posts") ? 'active' : ''}}" href="/blog">Blog</a>
+          <a class="nav-link {{ ($active === "posts") ? 'active' : ''}}" href="/blog">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "categories") ? 'active' : ''}}" href="/categories">Categories</a>
+          <a class="nav-link {{ ($active === "categories") ? 'active' : ''}}" href="/categories">Categories</a>
         </li>
       </ul>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => 'about',
         "nama" => 'Mohamad Egi Rahayu',
         "email" => "egirahayu@gmail.com",
         "image" => "Egi Rahayu.JPG"
@@ -47,20 +49,5 @@ Route::get('/categories', function () {
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post by Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post by Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
     ]);
 });
