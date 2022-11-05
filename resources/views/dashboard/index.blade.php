c90902e7dd393258d70c339f85630b33de11d958
Egirahayu <mohammadegirahayu>
2022-11-05 14:32:49 +0700

    Membuat user login dan middleware

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome, Egi Rahayu</h1>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -26,10 +26,28 @@
       </ul>
 
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link {{ ($active === "login") ? 'active' : ''}}" href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-        </li>
-      </ul>      
+          @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Welcome back, {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-right"></i> Login</button>
+                </form>
+              </li>
+            </ul>
+          </li>
+          @else
+          <li class="nav-item">
+            <a class="nav-link {{ ($active === "login") ? 'active' : ''}}" href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          </li>
+          @endauth       
+      </ul>
     </div>
   </div>
 </nav>
