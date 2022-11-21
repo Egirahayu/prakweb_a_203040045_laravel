66124ab4f37dcca1f59e0d70f36551c7b8f939ba
Egirahayu <mohammadegirahayu>
2022-11-21 14:38:22 +0700

    Membuat create post form

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -10,6 +10,15 @@
     
 <!-- Custom styles for this template -->
 <link href="/css/dashboard.css" rel="stylesheet">
+
+{{-- Trix --}}
+<link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0-beta.0/dist/trix.css">
+
+<style>
+  trix-toolbar [data-trix-button-group="file-tools"] {
+    display: none;
+  }
+</style>
 </head>
 <body>
   @include('dashboard.layouts.header')
@@ -23,6 +32,7 @@
     </div>
   </div>
 
+  <script type="text/javascript" src="https://unpkg.com/trix@2.0.0-beta.0/dist/trix.umd.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
   <script src="/js/dashboard.js"></script>
